22a57047f078255915178469a8f08dffda77863b
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-10 16:36:10 -0300

    ng for

--- aprendendo-angular/src/app/pages/home/home.component.ts
+++ aprendendo-angular/src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { UserCardComponent } from '../../components/user-card/user-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UserCardComponent],
+  imports: [CommonModule, UserCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  example = "Texto de exemplo";
-  userA = { name: "Alex", email: "alex@email.com", job: "Instrutor de Programação"}
-  userB = { name: "Osvaldo", email: "osvaldo@email.com", job: "Estudante"}
+  example = 'Texto de exemplo';
+  userList = [
+    { name: 'Alex', email: 'alex@email.com', job: 'Instrutor de Programação' },
+    { name: 'Osvaldo', email: 'osvaldo@email.com', job: 'Estudante' },
+    {
+      name: 'Maria',
+      email: 'maria@email.com',
+      job: 'Desenvolvedora Front-end',
+    },
+  ];
 }
