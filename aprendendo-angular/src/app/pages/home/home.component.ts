a59fadc39fab8a98bf6961ab0d3874e789136954
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-10 12:20:27 -0300

    props first example

--- aprendendo-angular/src/app/pages/home/home.component.ts
+++ aprendendo-angular/src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { UserCardComponent } from '../../components/user-card/user-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
   example = "Texto de exemplo";
+  userA = { name: "Alex", email: "alex@email.com", job: "Instrutor de Programação"}
+  userB = { name: "Osvaldo", email: "osvaldo@email.com", job: "Estudante"}
 }
